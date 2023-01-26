71e89379d63e29ac4ceec54af9c9dbd7b9395be8
prd-xxx <hoge>
2023-01-26 21:13:29 +0900

    [0_1_2] sample_0_1_2からコピー

---
+++ 0_1_2/prd_py_TLE/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+N = int(input())
+A = list(map(int,input().split()))
+
+ans = [0] * 3
+for i in range(N-1):
+    for j in range(i+1, N):
+        arr = A[i:j+1]
+        if 0 in arr:
+            ans[0] += 1
+        elif all(a==1 for a in arr):
+            ans[1] += 1
+        elif arr.count(2) == 1:
+            ans[2] += 1
+print(*ans)
