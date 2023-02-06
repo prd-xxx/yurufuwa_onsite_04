fe964b5cf17864ce9ade75e28d07351b7fca5588
ayaokondo <kondou.ayao@gmail.com>
2023-02-07 02:12:59 +0900

    validator の追加

---
+++ yurufuwa/tests/validator_2.cpp
@@ -0,0 +1,11 @@
+#include "testlib.h"
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    registerValidation();
+    inf.readToken("[a-z]{8}");
+    inf.readEoln();
+    inf.readEof();
+}
